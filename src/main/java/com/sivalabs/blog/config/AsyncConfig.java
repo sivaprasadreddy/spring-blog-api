4e4383714bfe429ae6c29a23d99e6f8f8caa70ed
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2026-07-01 22:06:32 +0530

    refactor: modularize configuration: move scheduling and async-related annotations to dedicated config classes; reorder application.properties sections

---
+++ src/main/java/com/sivalabs/blog/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.sivalabs.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+}
